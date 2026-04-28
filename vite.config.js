81b59cec49211c5abd0340205fa7bb3c886e519a
Tarique35 <ansaritarique35@gmail.com>
2026-04-28 21:27:26 +0530

    feat: add gh-pages deployment scripts and configure base path for production hosting

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/portfolio_2026/',
   server: {
     host: true,
     port: 5173,
